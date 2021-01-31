d16102b101ccb0ffc0e82ff1d2a9866e814e26b1
Sten Sipma <sten.sipma@ziggo.nl>
2021-01-31 09:49:07 +0100

    Proper highlighting for both menus

--- lua/fits.lua
+++ lua/fits.lua
@@ -1,5 +1,21 @@
 local menu = require('menu')
 
+local function text_center(str, width)
+        if not width then
+                width = vim.bo.textwidth
+        end
+
+        local space = math.max(width - #str, 0)
+        if space == 0 then
+                return str
+        end
+
+        local pad = " "
+        local l = math.floor(space/2)
+        local r = math.ceil(space/2)
+        return string.rep(pad, l) .. str .. string.rep(pad, r)
+end
+
 local function info_to_string(info)
         local hdu_type  = info[4]
         if hdu_type == "BinTableHDU" then
@@ -25,6 +41,55 @@ local function match_highlight(group, regex, str, line, offset)
         end
 end
 
+local type_ident_map = {
+        boolean = "Boolean",
+        string = "String",
+        number = "Number",
+}
+
+local function header_highlighting(lines, header)
+        local cmt_regex = vim.regex(' / .*$') -- Matches a comment at the end
+        for i,v in ipairs(lines) do
+                local value_identifier = type_ident_map[type(header[i][2])]
+                vim.api.nvim_buf_add_highlight(0, -1, value_identifier, i-1, 10, -1)
+                vim.api.nvim_buf_add_highlight(0, -1, 'Identifier', i-1, 0, 8)
+                vim.api.nvim_buf_add_highlight(0, -1, 'Comment', i-1, 9, 10)
+                match_highlight('Comment', cmt_regex, v, i)
+        end
+end
+
+local function info_highlighting(lines)
+        local type_regex = vim.regex(": .*$")
+        vim.api.nvim_buf_add_highlight(0, -1, 'Identifier', 0, 0, -1)
+        vim.api.nvim_buf_add_highlight(0, -1, 'Comment', 1, 0, -1)
+        for i=3,#lines do
+                vim.api.nvim_buf_add_highlight(0, -1, 'Comment', i-1, 0, -1)
+                vim.api.nvim_buf_add_highlight(0, -1, 'Identifier', i-1, 0, 16)
+                vim.api.nvim_buf_add_highlight(0, -1, 'Number', i-1, 19, 30)
+                vim.api.nvim_buf_add_highlight(0, -1, 'String', i-1, 34, -1)
+                match_highlight('Type', type_regex, lines[i], i, {1, 0})
+        end
+end
+
+local function header_settings(bufname, buf)
+        if not buf then
+                buf = vim.api.nvim_get_current_buf()
+        end
+        vim.api.nvim_buf_set_name(buf, bufname)
+
+        vim.api.nvim_buf_set_option(0, 'buftype', 'nofile')
+        vim.api.nvim_buf_set_option(0, 'swapfile', false)
+        vim.api.nvim_buf_set_option(0, 'filetype', 'FITS-header')
+        vim.api.nvim_buf_set_option(0, 'bufhidden', 'wipe')
+
+        local opts = {noremap=true, nowait=true}
+        vim.api.nvim_buf_set_keymap(0, "n", "q", ":lua vim.api.nvim_win_close(0, true)<cr>", opts)
+
+        vim.wo.wrap = false
+        vim.wo.cursorline = true
+
+end
+
 local function display_header(filename)
         return function ()
                 local line = vim.api.nvim_win_get_cursor(0)[1]
@@ -43,26 +108,8 @@ local function display_header(filename)
                         lines[#lines+1] = string.format("%-8s | %-40s", v[1], v[2]) .. to_append
                 end
                 vim.api.nvim_buf_set_lines(buf, 0, -1, true, lines)
-                vim.api.nvim_buf_set_name(buf, "Header "..tostring(header_idx).." "..filename)
-
-                vim.api.nvim_buf_set_option(0, 'buftype', 'nofile')
-                vim.api.nvim_buf_set_option(0, 'swapfile', false)
-                vim.api.nvim_buf_set_option(0, 'filetype', 'FITS-header')
-                vim.api.nvim_buf_set_option(0, 'bufhidden', 'wipe')
-
-                local opts = {noremap=true, nowait=true}
-                vim.api.nvim_buf_set_keymap(0, "n", "q", ":lua vim.api.nvim_win_close(0, true)<cr>", opts)
-
-                vim.wo.wrap = false
-                vim.wo.cursorline = true
-
-                -- highlighting
-                local cmt_regex = vim.regex(' / .*$')
-                local key_regex = vim.regex('.* |')
-                for i,v in ipairs(lines) do
-                        match_highlight('String', cmt_regex, v, i)
-                        match_highlight('Identifier', key_regex, v, i, {0, -1})
-                end
+                header_settings("Header "..tostring(header_idx).." "..filename, buf)
+                header_highlighting(lines, header)
         end
 end
 
@@ -78,10 +125,10 @@ local function inspect_fits(filename)
         local mlines = {}
         local path_list = vim.split(filename, '/')
 
-        lines[1] = "| " .. path_list[#path_list] .. " |"
+        lines[1] = text_center(path_list[#path_list])
         mlines[1] = menu.do_nothing
 
-        lines[2] = string.rep("-", #lines[1])
+        lines[2] = string.rep("-", vim.bo.textwidth)
         mlines[2] = menu.do_nothing
 
         local offset = 2
@@ -92,10 +139,10 @@ local function inspect_fits(filename)
 
 
         menu.picker(lines, mlines)
+        info_highlighting(lines)
 end
 
 return {
         inspect_fits = inspect_fits,
         display_header = display_header,
 }
-
